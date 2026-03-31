827939cdae4a5ca7208c068c0f4e8a6a35d6f476
Omar Faruk <omarfaruk.dev@gmail.com>
2026-03-31 18:18:02 +0600

    auth layout added

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
  
 export default function App() {
@@ -7,6 +8,15 @@ export default function App() {
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary px-4 py-2 text-white">
+        Go to Onboarding 
+      </Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary px-4 py-2 text-white">
+        Go to Sign In 
+      </Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary px-4 py-2 text-white">
+        Go to Sign Up 
+      </Link>
     </View>
   );
 }
